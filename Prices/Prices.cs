6c8d9c719a7a183963b5c2f855b02c7bd1a8fb17
Jenny Ramsay <jenny.ramsay@7digital.com>
2016-06-06 15:08:20 +0100

    Initial commit with a dll

---
+++ Prices/Prices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stockroom
+{
+    public class Prices
+    {
+        
+        private Random random = new Random();
+        public virtual double PriceFor(string item)
+        {
+            if (item.ToLower().StartsWith("a"))
+            {
+                return 5.5;
+            } else
+            {
+                return Math.Round(random.NextDouble() * 100, 2);
+            }
+        }
+    }
+}
